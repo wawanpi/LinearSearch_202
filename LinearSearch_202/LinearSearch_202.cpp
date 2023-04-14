5f7588e5e2190713b48d8c16b9cdee81b6aa406e
wawanpi <muhaguskurniawan@gmail.com>
2023-04-14 19:10:17 +0700

    memberikan nialn dan i

--- LinearSearch_202/LinearSearch_202.cpp
+++ LinearSearch_202/LinearSearch_202.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];				////Array to be search
+int arr[20];				//Array to be search
+int n;						//Number of element in the array
+int i;						//Index of array element
