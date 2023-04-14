a2aa53af585a85aff1dff17023a5d4fabd9eb892
wawanpi <muhaguskurniawan@gmail.com>
2023-04-14 19:15:55 +0700

    membuat input

--- LinearSearch_202/LinearSearch_202.cpp
+++ LinearSearch_202/LinearSearch_202.cpp
@@ -4,3 +4,12 @@ using namespace std;
 int arr[20];				//Array to be search
 int n;						//Number of element in the array
 int i;						//Index of array element
+
+void input() 
+{
+	while (true)
+	{
+		cout << "Enter the number of elements in the Array = ";
+
+	}
+}
