6bec989803d26322599d7ba1606d01d0bf2dd458
wawanpi <muhaguskurniawan@gmail.com>
2023-04-14 19:55:29 +0700

    menampilkan comparisson

--- LinearSearch_202/LinearSearch_202.cpp
+++ LinearSearch_202/LinearSearch_202.cpp
@@ -53,6 +53,7 @@ void LinearSearch()
 		if (i == n)					//Langkah 5b not found
 		{
 			cout << "\n" << item << "Not Found in the Array\n";
+			cout << "\nNumber of comparisson : " << comparisson << endl;
 		}
 }
 
